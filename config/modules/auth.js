3adf1c4f1d73211adf6983f0a706a8b8f5f40744
Alexander Lashchevsky <alexander@lashchevsky.me>
2022-05-10 16:35:27 +0400

    Add auth refresh token

--- config/modules/auth.js
+++ config/modules/auth.js
@@ -7,14 +7,20 @@ export default {
   },
   strategies: {
     local: {
+      scheme: 'refresh',
       token: {
         property: 'response.accessToken'
+      },
+      refreshToken: {
+        property: 'response.refreshToken',
+        data:     'refreshToken'
       },
       user: {
         property: 'response.user'
       },
       endpoints: {
         login:    { method: 'POST', url: '/api/auth' },
+        refresh:  { method: 'POST', url: '/api/auth/refresh' },
         user:     { method: 'GET',  url: '/api/auth/user' }
       }
     }
